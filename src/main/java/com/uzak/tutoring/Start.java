b1ab0de91ea2b95478ae00ea66c4c9622b9c650f
liangxiudou <1023378931@qq.com>
2018-03-28 02:07:54 +0800

    初始化项目

---
+++ src/main/java/com/uzak/tutoring/Start.java
@@ -0,0 +1,19 @@
+package com.uzak.tutoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EntityScan(basePackages= {"com.uzak.tutoring.dao"})
+@ComponentScan(basePackages= {"com.uzak.tutoring"})
+public class Start {
+	public static void main(String[] args) {
+		SpringApplication.run(Start.class, args);
+	}
+}
---
+++ src/main/java/com/uzak/tutoring/dao/User.java
@@ -0,0 +1,45 @@
+package com.uzak.tutoring.dao;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "User")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long iD;
+	@Column
+	private String name;
+	@Column
+	private Integer age;
+
+	public Long getId() {
+		return iD;
+	}
+
+	public void setId(Long id) {
+		this.iD = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+}
